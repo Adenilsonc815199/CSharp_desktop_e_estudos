288b7d616ddf28af813cb09534512f8d36635f76
Adenilson Cardoso <adenilson.csantos@hotmail.com>
2023-09-14 17:42:17 -0300

    Add files via upload
    Aplicações de estudo com calculos de média e exercicio de Fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Obtendo numero digitado
+            Console.WriteLine("Insira um numero inteiro: ");
+                string numeroEntrada = Console.ReadLine();
+                int numeroConvertido = Int32.Parse(numeroEntrada);
+                int numeroAnterior = 0;
+                int numeroAtual = 1;
+                int fibonacci;
+                bool existeNaSequencia = false;
+                List<int> numerosAnteriores = new List<int>();
+
+            for (int i = 0; i < numeroConvertido; i++)
+                {
+                //Imprimindo a sequencia, e inserindo numero anterior na lista
+                Console.WriteLine(numeroAnterior);
+                numerosAnteriores.Add(numeroAnterior);
+
+                fibonacci = numeroAnterior + numeroAtual;
+                numeroAnterior = numeroAtual;
+                numeroAtual = fibonacci;
+
+                //Valida se numero informado existe em algum momento do calculo
+                if(numeroConvertido == numeroAnterior || numeroConvertido == numeroAtual || numeroConvertido == fibonacci)
+                {
+                    existeNaSequencia = true;
+                }
+            }
+
+            //Resposta se existe ou não
+            if(existeNaSequencia == true)
+            {
+                Console.WriteLine("Numero informado existe na sequencia de Fibonacci, numero: " + numeroConvertido);
+            }
+            else
+            {
+                Console.WriteLine("Numero informado Não existe na sequencia de Fibonacci, numero: " + numeroConvertido);
+            }
+
+
+        }
+    }
+
+}
---
+++ calculoMedia/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsultaVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double media;
+            double SP = 67836.43;
+            double RJ = 36678.66;
+            double MG = 29229.88;
+            double ES = 27165.48;
+            double Outros = 19849.53;
+
+            media = SP + RJ + MG + ES + Outros;
+
+            double mediaSP = (SP / media)* 100;
+            double mediaRJ = (RJ / media) * 100;
+            double mediaMG = (MG / media) * 100;
+            double mediaES = (ES / media) * 100;
+            double mediaOutros = (Outros / media) * 100;
+            Console.WriteLine("Média de SP " + mediaSP.ToString("N2") + "%");
+            Console.WriteLine("Média de RJ " + mediaRJ.ToString("N2") + "%");
+            Console.WriteLine("Média de MG" + mediaMG.ToString("N2") + "%");
+            Console.WriteLine("Média de RS " + mediaES.ToString("N2") + "%");
+            Console.WriteLine("Média dos Outros " + mediaOutros.ToString("N2") + "%");
+        }
+    }
+}
